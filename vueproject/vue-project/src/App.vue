cbd4888c1264f63a93837efd764de59a1d463606
gosse <2519874285@qq.com>
2024-11-24 22:09:47 +0800

    前端工程化vue-project P10

---
+++ vueproject/vue-project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app"><!-- 用于css -->
+    <element-view></element-view>
+  </div>
+</template>
+
+<script>
+import ElementView from './views/element/ElementView.vue'
+export default {
+  components: { ElementView },
+  data:function(){
+    return {
+      message:"Hello Vue"
+    }
+  }
+}
+</script>
+
+
+<style>/* css */
+
+</style>
---
+++ vueproject/vue-project/src/views/element/ElementView.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <!-- button组件 -->
+    <el-row>
+      <el-button round>圆角按钮</el-button>
+      <el-button type="primary" round>主要按钮</el-button>
+      <el-button type="success" round>成功按钮</el-button>
+      <el-button type="info" round>信息按钮</el-button>
+      <el-button type="warning" round>警告按钮</el-button>
+      <el-button type="danger" round>危险按钮</el-button>
+    </el-row>
+
+    <!-- table表格组件 -->
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
+
+    <!-- pagination分页组件 -->
+    <el-pagination
+      background
+      layout="sizes, prev, pager, next, jumper"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :total="1000"
+    ></el-pagination>
+
+    <!-- dialog对话框组件 -->
+    <el-button type="text" @click="dialogVisible = true"
+      >点击打开 Dialog</el-button
+    >
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialogVisible:false,
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "tom",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "gosse",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王2",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleSizeChange: function (val) {
+      alert("每页大小变化为：" + val);
+    },
+    handleCurrentChange: function (val) {
+      alert("当前页数变化为：" + val);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
